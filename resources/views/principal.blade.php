352930e17079743924e663c87986e67a36b14a06
blackboxtecno <blackboxtectux@gmail.com>
2024-01-29 19:20:39 -0600

    cambios de redes sociales
    se añadieron las redes sociales

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -6,7 +6,9 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
-    <title>Cover Template · Bootstrap v5.0</title>
+    <title>Desarollo web-BlackBox</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('static/css/style.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
 
@@ -14,6 +16,39 @@
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
+        
+html,body{
+display: grid;
+height: 100%;
+width: 100%;
+font-family: "Poppins", sans-serif;
+place-items: center;
+background: black;
+}
+.wrapper .icon{
+    position: relative;
+    background: #000000;
+    border-radius: 50%;
+    padding: 15px;
+    margin: 10px;
+    width: 50px;
+    height: 50px;
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    box-shadow: 0 10px 10px rgba(0,0,0,0.1);
+    cursor: pointer;
+  transition: all 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+
+.wrapper{
+    display: inline-flex;
+    list-style: none;
+    margin-top: 15px;
+}
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -38,10 +73,23 @@
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
-      <h3 class="float-md-start mb-0">Black box</h3>
+      <h3 class="float-md-start mb-0" style="margin-top: 15px;">Black box</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link active" aria-current="page" href="#">Instagram</a>
-        <a class="nav-link" href="#">Tik tok</a>
+      <ul class="wrapper">
+        <li class="icon facebook">
+            <span class="tooltip">Facebook</span>
+            <span><i class="fab fa-facebook-f"></i></span></li>
+            <li class="icon tiktok">
+            <span class="tooltip">Tiktok</span>
+            <span><i class="fab fa-tiktok"></i></span></li>
+        <li class="icon instagram">
+            <span class="tooltip">Instagram</span>
+            <span><i class="fab fa-instagram"></i></span></li>
+        <li class="icon github">
+            <span class="tooltip">Github</span>
+            <span><i class="fab fa-github"></i></span></li>
+       
+    </ul>
       <!--  <a class="nav-link" href="#">Trabajos realizados</a>-->
       </nav>
     </div>
@@ -56,7 +104,7 @@
   </main>
 
   <footer class="mt-auto text-white-50">
-    <p>Siguenos en nuestras redes como <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, y <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+    <p>Siguenos en nuestras redes como <a href="https://getbootstrap.com/" class="text-white">  Instagram</a>, y <a href="https://www.tiktok.com/@blackboxtec?is_from_webapp=1&sender_device=pc" class="text-white">Tik tok</a>.</p>
   </footer>
 </div>
 
